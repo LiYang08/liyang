49907ca1fa7af4b5b94b4d8d0fdaa44d14d5189b
LiYang08 <15615540541@163.com>
2020-08-15 23:36:55 +0800

    air.html V1.0

---
+++ gulp-cli/dist/script/header.js
@@ -0,0 +1,40 @@
+/* 数据加载 */
+$('.dist_a').on("mouseenter","a" ,function () {
+    $('.cate_con').html('');
+    let $a = $(this).index();
+    var cateDom='';
+    $('.category_li2').css('display','block');
+    $.ajax({
+        type: "get",
+        url: "../data/header.json",
+        data: "data",
+        dataType: "json",
+        success: function (response) {
+            const obj = response[$a];
+            for(var key in obj){
+            cateDom += `<div class="cate_case">
+                <h3>${key}</h3>
+                <ul class="cate_menu clearBoth">`;
+                var arr = obj[key];
+                arr.forEach(item => {
+                    cateDom +=`<li><a href="#">${item}</a></li>`;
+                });
+                cateDom +=`</ul></div>`;
+            }
+           $('.cate_con').append(cateDom);
+            let $h =$(".cate_con").height();
+            if($h>200){
+                $(".cate_img").css('display','none');
+            }else{
+                $(".cate_img").css('display','block');
+            }
+        }
+    });
+});
+/* input 获取焦点*/
+$("#inp_s").focus(function(){
+    $(this).css("border","1px solid #c8c8c8");
+});
+$("#inp_s").blur(function(){
+    $(this).css("border","1px solid #ffffff");
+});
--- gulp-cli/dist/script/index-29cb968e38.js
+++
@@ -1,1 +0,0 @@
-"use strict";$("#header").load("./header.html"),$("#banner").load("./banner.html"),$("#xinxuan").load("./xinxuan.html"),$("#content").load("./content.html"),$("#footer").load("./footer.html");
